8a76bac3a77d7fc59c650d0b6ceac05c4625f469
Ali Ahmadabadiha <Ahmadabadiha@gmail.com>
2022-03-21 10:13:40 +0330

    3rd commit

--- app/src/main/java/com/example/tabbedproject/ToDoFragment.kt
+++ app/src/main/java/com/example/tabbedproject/ToDoFragment.kt
@@ -5,55 +5,168 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tabbedproject.data.Task
+import com.example.tabbedproject.databinding.FragmentToDoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ToDoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ToDoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_open) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_close) }
+    private val toButtom: Animation by lazy { AnimationUtils.loadAnimation(requireContext(), R.anim.to_bottom) }
+    private val fromButtom: Animation by lazy { AnimationUtils.loadAnimation(requireContext(), R.anim.from_bottom) }
+    private val sharedViewModel: SharedViewModel by activityViewModels { TaskViewModelFactory((requireActivity().application as TaskApplication).repository) }
+    private var todoTasks: List<Task> = emptyList()
+    private var clicked = false
+    private lateinit var myAdapter: TaskAdapter
+    private var _binding: FragmentToDoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do, container, false)
+    ): View {
+        _binding = FragmentToDoBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ToDoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ToDoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedViewModel.getUserTasks(sharedViewModel.username)
+        initClickListeners()
+        myAdapter = TaskAdapter(todoTasks)
+
+        sharedViewModel.taskList.observe(viewLifecycleOwner) {
+            binding.noTaskView.isVisible = it.isEmpty()
+
+            todoTasks = it.filter {
+                it.state == "ToDo"
             }
+
+            myAdapter = TaskAdapter(todoTasks)
+            setRecyclerAdapter(myAdapter)
+
+
+        }
+
+        sharedViewModel.searchCouple.observe(viewLifecycleOwner) {
+            applySearch()
+        }
+    }
+
+    private fun initClickListeners() {
+        binding.openFloatingButton.setOnClickListener {
+
+            if (!clicked) {
+                openFabs()
+
+            } else {
+                closeFabs()
+
+            }
+        }
+        binding.addFloatingButton.setOnClickListener {
+            val dialog = AddTaskDialog("ToDo")
+            dialog.show(childFragmentManager, "Add task dialog")
+        }
+        binding.searchFloatingButton.setOnClickListener {
+            val dialog = SearchTaskDialog()
+            dialog.show(childFragmentManager, "Search task dialog")
+        }
+
+        binding.deleteAllFloatingButton.setOnClickListener {
+            DeleteAllDialog().show(childFragmentManager, "Delete all dialog")
+
+        }
+    }
+
+    private fun openFabs() {
+        binding.addFloatingButton.apply {
+            isVisible = true
+            isClickable = true
+            startAnimation(fromButtom)
+        }
+
+        binding.searchFloatingButton.apply {
+            isVisible = true
+            isClickable = true
+            startAnimation(fromButtom)
+        }
+        binding.deleteAllFloatingButton.apply {
+            isVisible = true
+            isClickable = true
+            startAnimation(fromButtom)
+        }
+
+        binding.openFloatingButton.startAnimation(rotateOpen)
+        clicked = !clicked
+    }
+
+    private fun closeFabs() {
+        binding.addFloatingButton.apply {
+            isGone = true
+            isClickable = false
+            startAnimation(toButtom)
+
+        }
+        binding.searchFloatingButton.apply {
+            isGone = true
+            isClickable = false
+            startAnimation(toButtom)
+        }
+
+        binding.deleteAllFloatingButton.apply {
+            isGone = true
+            isClickable = false
+            startAnimation(toButtom)
+        }
+        binding.openFloatingButton.startAnimation(rotateClose)
+
+        clicked = !clicked
+    }
+
+    private fun setRecyclerAdapter(myAdapter: TaskAdapter) {
+        myAdapter.onViewClicked(object : TaskAdapter.ItemClick {
+            override fun onClick(task: Task) {
+                val dialog = EditTaskDialog(task)
+                dialog.show(childFragmentManager, "Edit task dialog")
+            }
+        })
+        binding.recyclerView.apply {
+            adapter = myAdapter
+            layoutManager = LinearLayoutManager(context)
+            myAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun applySearch() {
+
+        val searchCouple = sharedViewModel.searchCouple.value!!
+        val searchedAttribute = searchCouple.first
+        val query = searchCouple.second
+        if (searchedAttribute.isBlank() || query.isBlank()) setRecyclerAdapter(myAdapter)
+        else {
+            var searchedTask = listOf<Task>()
+            when (searchedAttribute) {
+                "title" -> searchedTask = todoTasks.filter { it.title.contains(query, true) }
+                "description" -> searchedTask = todoTasks.filter { it.description.contains(query, true) }
+                "date" -> searchedTask = todoTasks.filter { it.date.contains(query, true) }
+                "time" -> searchedTask = todoTasks.filter { it.time.contains(query, true) }
+            }
+            setRecyclerAdapter(TaskAdapter(searchedTask))
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
